5279ab61224242e5743b575a2f277b5cf6134dd3
RusinovArtem <ar@moderntoken.com>
2018-11-16 16:15:53 +0300

    comments

--- src/Exchange/Order.php
+++ src/Exchange/Order.php
@@ -6,13 +6,13 @@ namespace Crypto\Exchange;
 
 class Order
 {
-    public $id;
-    public $date;
+    public $id = null; //Идентификатор ордера на бирже
+    public $date; //Дата размещения ордера на бирже
     public $price;
     public $value;
-    public $pairID;
-    public $side;
-    public $traded = 0;
+    public $pairID; //И
+    public $side; //"buy" or "sell"
+    public $traded = 0; //Количество проданых\купленных монет
     public $status; // new, suspended, partiallyFilled, filled, canceled, expired
 
     public function isActive()
