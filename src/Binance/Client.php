2e9e32b45b0e33d88d91136ca2bb630cb86f5bb9
RusinovArtem <ar@moderntoken.com>
2018-12-11 13:30:30 +0300

    Binance signed query

--- src/Binance/Client.php
+++ src/Binance/Client.php
@@ -8,9 +8,16 @@ use Crypto\Exchange\ClientInterface;
 use Crypto\Exchange\CurrencyBalance;
 use Crypto\Exchange\Order;
 use Crypto\Exchange\Pair;
+use Crypto\Exchange\PairLimit;
+use Crypto\Traits\Loggable;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use Monolog\Logger;
 
 class Client implements ClientInterface
 {
+
+    use Loggable;
 
     public $apiKey;
     public $secretKey;
@@ -20,12 +27,59 @@ class Client implements ClientInterface
      */
     public $client;
 
+    public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client();
+    }
+
     /**
      * @return Pair[]
      */
     public function getPairs()
     {
        $response = $this->request("GET", "exchangeInfo", []);
+
+       if($response->getStatusCode() == 200)
+       {
+         $data = json_decode((string)$response->getBody(), true);
+
+         $result = [];
+
+         foreach ($data['symbols'] as $symbol)
+         {
+            $pair = new Pair();
+            $pair->id = $symbol['symbol'];
+            $pair->baseCurrency = $symbol['baseAsset'];
+            $pair->quoteCurrency = $symbol['quoteAsset'];
+
+            $limit = new PairLimit();
+            $limit->pairID = $pair->id;
+
+            foreach ($symbol['filters'] as $filter)
+            {
+                if($filter['filterType'] === 'PRICE_FILTER')
+                {
+                    $limit->priceTick = $filter['tickSize'];
+                }
+
+                if($filter['filterType'] === 'LOT_SIZE')
+                {
+                    $limit->lotSize = $filter['stepSize'];
+                    if($filter['stepSize'] !== $filter['minQty'])
+                    {
+                        var_dump($symbol);
+                    }
+                }
+            }
+
+            $pair->limit = $limit;
+            $result[$pair->id] = $pair;
+         }
+
+            return $result;
+       }
+
+
     }
 
     /**
@@ -105,11 +159,15 @@ class Client implements ClientInterface
     {
 
         $dataToSend = [];
+        $params['timestamp'] = number_format(microtime(true) * 1000, 0, '.', '');
+        $query = http_build_query($params, '', '&');
+        $signature = hash_hmac('sha256', $query, $this->secretKey);
+        $params['signature'] = $signature;
 
-        $dataToSend['auth'] = [
-            $this->apiKey,
-            $this->secretKey
-        ];
+        $dataToSend['headers'] =
+            [
+              "X-MBX-APIKEY" => $this->apiKey,
+            ];
 
         if(strtolower($method) !== "get")
         {
@@ -121,7 +179,25 @@ class Client implements ClientInterface
         }
 
 
-        $response = $this->client->request($method, "https://api.binance.com/api/v1/" . $action, $dataToSend);
+        try {
+            $jParams = json_encode($params);
+            $response = $this->client->request($method, "https://api.binance.com/api/v3/" . $action, $dataToSend);
+        } catch (ClientException $e) {
+            $eResponse = $e->getResponse();
+            $logMessage = "REQUEST {$method} {$action} with params $jParams";
+            $logMessage .= "\n\t RESPONSE status=" . $eResponse->getStatusCode() . " body=" . (string)$eResponse->getBody();
+            $this->log($logMessage, [], Logger::ERROR);
+
+            return $eResponse;
+
+        } catch (ServerException $e) {
+            $eResponse = $e->getResponse();
+            $logMessage = "REQUEST {$method} {$action} with params $jParams";
+            $logMessage .= "\n\t RESPONSE status=" . $eResponse->getStatusCode() . " body=" . (string)$eResponse->getBody();
+            $this->log($logMessage, [], Logger::ERROR);
+
+            return $eResponse;
+        }
 
         return $response;
     }
